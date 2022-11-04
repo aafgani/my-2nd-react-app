a1103ead5ae307ab10741601debf0ebe7a482af2
aafgani <andrya.afgani@gmail.com>
2022-11-04 11:05:04 +0700

    add router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cocktail from "./container/Cocktail";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/cocktail" element={<Cocktail />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
